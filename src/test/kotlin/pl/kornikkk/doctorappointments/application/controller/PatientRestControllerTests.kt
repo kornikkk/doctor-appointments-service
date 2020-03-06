de659ab360b1cf8ef2e6a7e39f84e6a3f76e6d55
kornikkk <kornik1992@gmail.com>
2020-03-06 17:08:20 +0100

    REST API  * Updated Patient REST controller tests

--- src/test/kotlin/pl/kornikkk/doctorappointments/application/controller/PatientRestControllerTests.kt
+++ src/test/kotlin/pl/kornikkk/doctorappointments/application/controller/PatientRestControllerTests.kt
@@ -4,6 +4,8 @@ import com.ninjasquad.springmockk.MockkBean
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.spring.SpringListener
 import io.mockk.every
+import io.mockk.mockk
+import org.hamcrest.core.StringContains
 import org.hamcrest.core.StringEndsWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -12,10 +14,10 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import pl.kornikkk.doctorappointments.domain.Patient
+import pl.kornikkk.doctorappointments.domain.exception.PatientNotFoundException
 import pl.kornikkk.doctorappointments.domain.service.PatientService
 import java.util.*
 
@@ -38,11 +40,8 @@ class PatientRestControllerTests : StringSpec() {
             val firstName = "Test"
             val lastName = "User"
             val address = "Street 2/3 City"
-            val patient = Patient(personId, firstName, lastName, address)
 
-            every { service.createPatient(firstName, lastName, address) } returns patient
-
-            val request = """
+            val requestBody = """
                 |{
                 |  "firstName" : "$firstName",
                 |  "lastName" : "$lastName",
@@ -50,12 +49,60 @@ class PatientRestControllerTests : StringSpec() {
                 |}
                 |""".trimMargin()
 
+            every {
+                service.createPatient(firstName, lastName, address)
+            } returns Patient(personId, firstName, lastName, address)
+
             mockMvc.perform(
                     post("/patients")
                             .contentType(MediaType.APPLICATION_JSON)
-                            .content(request))
+                            .content(requestBody))
                     .andExpect(status().isCreated)
                     .andExpect(header().string(HttpHeaders.LOCATION, StringEndsWith(true, personId.toString())))
+        }
+
+        "patients PUT should update patient" {
+            val personId = UUID.randomUUID()
+
+            val requestBody = """
+                |{
+                |  "firstName" : "Test",
+                |  "lastName" : "Patient",
+                |  "address" : "Street 2/3 City"
+                |}
+                |""".trimMargin()
+
+            every { service.updatePatient(any()) } returns mockk()
+
+            mockMvc.perform(
+                    put("/patients/$personId")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody))
+                    .andExpect(status().isOk)
+        }
+
+        "patiens GET should get patient" {
+            val personId = UUID.randomUUID()
+            val patient = mockk<Patient>(relaxed = true)
+
+            every { patient.personId } returns personId
+            every { service.getPatient(personId) } returns patient
+
+            mockMvc.perform(
+                    get("/patients/$personId"))
+                    .andExpect(status().isOk)
+                    .andExpect(content().string(StringContains(true, personId.toString())))
+
+        }
+
+        "patients GET should return NotFound when patient not existing" {
+            val personId = UUID.randomUUID()
+
+            every { service.getPatient(any()) } throws PatientNotFoundException(personId)
+
+            mockMvc.perform(
+                    get("/patients/$personId"))
+                    .andExpect(status().isNotFound)
         }
     }
 }
