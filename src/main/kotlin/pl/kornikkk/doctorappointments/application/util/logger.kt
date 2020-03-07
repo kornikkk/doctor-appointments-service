f74ecf29f947c17302a19211deee0d45a0751b0d
kornikkk <kornik1992@gmail.com>
2020-03-07 12:41:20 +0100

    Logging  * Added logger util

---
+++ src/main/kotlin/pl/kornikkk/doctorappointments/application/util/logger.kt
@@ -0,0 +1,9 @@
+package pl.kornikkk.doctorappointments.application.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory.getLogger
+
+interface Logging
+
+inline fun <reified T : Logging> T.logger(): Logger =
+        getLogger(T::class.java)
