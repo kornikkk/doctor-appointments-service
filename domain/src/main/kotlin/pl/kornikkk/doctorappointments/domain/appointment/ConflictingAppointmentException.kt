56e0ed8966851a7daadc061431f05def820d2465
kornikkk <kornik1992@gmail.com>
2020-03-11 21:01:02 +0100

    Appointment  * Changed error message

--- domain/src/main/kotlin/pl/kornikkk/doctorappointments/domain/appointment/ConflictingAppointmentException.kt
+++ domain/src/main/kotlin/pl/kornikkk/doctorappointments/domain/appointment/ConflictingAppointmentException.kt
@@ -7,4 +7,4 @@ import java.time.format.DateTimeFormatter.ISO_DATE_TIME
 import java.util.*
 
 class ConflictingAppointmentException(patientId: UUID, doctorId: UUID, date: LocalDate, time: LocalTime)
-    : Exception("Appointment for patient with id \"$patientId\" at doctor with id \"$doctorId\" at ${ISO_DATE_TIME.format(LocalDateTime.of(date, time))} would be conflicting another appointment")
+    : Exception("Appointment for patient with id $patientId at doctor with id $doctorId at ${ISO_DATE_TIME.format(LocalDateTime.of(date, time))} would be conflicting another appointment")
